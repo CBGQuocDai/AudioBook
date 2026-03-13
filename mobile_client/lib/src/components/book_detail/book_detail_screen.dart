79305bb509b978558c22d319514b39bb5534f8e6
Daniel Lee <>
2026-03-13 21:30:47 +0700

    demo cho ae

---
+++ mobile_client/lib/src/components/book_detail/book_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider/book_detail_provider.dart';
+import '../component/body.dart';
+
+class BookDetailScreen extends StatelessWidget {
+  const BookDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => BookDetailProvider(),
+      child: const Scaffold(
+        body: SafeArea(
+          child: BookDetailBody(),
+        ),
+      ),
+    );
+  }
+}
