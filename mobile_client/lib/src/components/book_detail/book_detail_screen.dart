a67c040a9c2557d9fa4f9ee8fc11209decd038fd
Daniel Lee <>
2026-03-13 22:25:11 +0700

    demo cho ae

--- mobile_client/lib/src/components/book_detail/book_detail_screen.dart
+++ mobile_client/lib/src/components/book_detail/book_detail_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../provider/book_detail_provider.dart';
-import '../component/body.dart';
+import 'provider/book_detail_provider.dart';
+import 'component/body.dart';
 
 class BookDetailScreen extends StatelessWidget {
   const BookDetailScreen({super.key});
