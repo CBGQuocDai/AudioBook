92612de52feeaebfe56159ef0bafced36d10c7b4
tungbdflexin <tungbd@flexin.tech>
2026-03-16 11:24:32 +0700

    init entity common

--- api/audio-book/src/main/java/org/backend/user/entity/User.java
+++ api/audio-book/src/main/java/org/backend/user/entity/User.java
@@ -1,5 +1,33 @@
 package org.backend.user.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldNameConstants;
+import org.backend.common.entity.SoftDeleteEntity;
+import org.backend.user.enums.RoleEnum;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldNameConstants
+public class User extends SoftDeleteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private RoleEnum role;
 }
 
